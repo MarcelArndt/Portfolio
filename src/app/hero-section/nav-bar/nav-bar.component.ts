212f5cc3397b25074e4bf528b34f5f8970dfcd9b
MarcelArndt <marcel.arndt@hotmail.com>
2024-09-22 10:17:16 +0200

    adding function to ColorSwitchButton and some responsiv work

--- src/app/hero-section/nav-bar/nav-bar.component.ts
+++ src/app/hero-section/nav-bar/nav-bar.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  isColorNavOn = false;
+  hideNavigation = true;
+  blueColorTheme = true;
 
+  toggleColorNav(){
+    this.isColorNavOn = !this.isColorNavOn;
+    if(!this.isColorNavOn){
+      setTimeout(() => {
+        this.hideNavigation = true
+      }, 325);
+    } else {
+      this.hideNavigation = false
+    }
+  }
+
+  switchToOrangeTheme(){
+    this.blueColorTheme = false;
+    console.log(this.blueColorTheme);
+  }
+
+  switchToBlueTheme(){
+    this.blueColorTheme = true;
+    console.log(this.blueColorTheme);
+  }
 }
