4abd8ade1ad6c04f817180b47ca94a83d5f2d577
MarcelArndt <marcel.arndt@hotmail.com>
2024-09-23 19:47:28 +0200

    some responsiv work.

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
+
 })
 export class AboutMeComponent {
 
