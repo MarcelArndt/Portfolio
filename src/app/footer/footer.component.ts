744ecd70b889182e2419c22a92a669d84d3a38af
MarcelArndt <marcel.arndt@hotmail.com>
2024-10-03 14:58:38 +0200

    finalize orange Theme to Website

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ColorSwapService } from '../color-swap.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  constructor(public ThemeColor: ColorSwapService){
 
+  }
 }
