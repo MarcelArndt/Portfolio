b8dbcb0639d8f2fc9fd0e4411cf0824b9a86ea25
MarcelArndt <marcel.arndt@hotmail.com>
2024-10-22 15:32:08 +0200

    adding 404-Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { MainComponentComponent } from './main-component/main-component.componen
 import { TempImprintComponent } from './temp-imprint/temp-imprint.component';
 import { TempProtectDataComponent } from './temp-protect-data/temp-protect-data.component';
 import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponentComponent },
     { path: 'imprint', component: TempImprintComponent },
-    { path: 'proctect-data', component: TempProtectDataComponent }
+    { path: 'proctect-data', component: TempProtectDataComponent},
+    { path: '**', component: NotFoundComponent },
+    { path: '404', component: NotFoundComponent },
 ];
 
 @NgModule({
